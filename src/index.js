24bdb4ff90ee469daa54ab0d80837b13e05b6143
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-09 23:42:45 +0100

    Include CustomUrl entry point

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@
  **/
 
 // Import the shortening function from shortener.js
-const shortener = require('./shortener');
+import { shorten } from './shortener'; 
+
 // Sample URL to shorten
 const longUrl = 'https://www.google.com/search?q=shorten+url&oq=shorten+url&aqs=chrome..69i57j0l7.2959j0j7&sourceid=chrome&ie=UTF-8';
-//Use the shortening function to shorten the URL
-const shortUrl = shortener.shorten(longUrl);
-//Finally, print the shortened URL
+
+// Custom URL code
+const customUrlCode = 'customCode';
+
+// Use the shortening function to shorten the URL
+const shortUrl = shorten(longUrl, customUrlCode);
+
+// Finally, print the shortened URL
 console.log(`Shortened URL: ${shortUrl}`);
+
