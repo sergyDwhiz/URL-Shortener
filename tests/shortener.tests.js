68018c6121445a8cf97679b9b484106fe90cc808
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-09 23:58:09 +0100

    Import App and Models

---
+++ tests/shortener.tests.js
@@ -0,0 +1,4 @@
+const request = require('supertest');
+const app = require('../app'); // Import your Express app
+const mongoose = require('mongoose');
+const Url = require('../models/url'); // Import your Url model
