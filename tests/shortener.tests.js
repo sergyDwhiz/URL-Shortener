55b8f24d35a91f402d419b96116403609e099055
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-09 23:58:55 +0100

    Connect Mongo test

--- tests/shortener.tests.js
+++ tests/shortener.tests.js
@@ -2,3 +2,11 @@ const request = require('supertest');
 const app = require('../app'); // Import your Express app
 const mongoose = require('mongoose');
 const Url = require('../models/url'); // Import your Url model
+beforeAll(async () => {
+    await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  });
