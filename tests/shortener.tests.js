4f9dc5491fa94cc6ee94a722ec2057fbd05cbfca
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-12 10:32:34 +0100

    Add unit test for POST shorten service

--- tests/shortener.tests.js
+++ tests/shortener.tests.js
@@ -1,7 +1,8 @@
-import request from ('supertest'); 
-const app = require('../app'); // Import your Express app import app from (../app);
+import request from 'supertest';
+import app from '../app.js'; // Import your Express app
 import mongoose from 'mongoose';
-import Url from ('../models/url');
+import Url from '../models/url.js'; // Import your Url model
+
 beforeAll(async () => { // Import your Url model 
     await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
       if (err) {
@@ -13,3 +14,15 @@ beforeAll(async () => { // Import your Url model
   afterAll(async () => {
     await mongoose.connection.close();
   });
+  describe('POST /shorten', () => {
+    it('should shorten a valid URL', async () => {
+      const res = await request(app)
+        .post('/shorten')
+        .send({
+          originalUrl: 'https://example.com',
+        });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('shortUrl');
+    }); 
+  });
+  
